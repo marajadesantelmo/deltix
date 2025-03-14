d8af1996863766292b8dc3cb32dd26348fe65274
Facu <facundol@hotmail.com>
2025-03-14 23:44:22 +0000

    hidrografia

--- scraping.py
+++ scraping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 #from PIL import Image
 import urllib.request
+from bs4 import BeautifulSoup
 
 #Scraping mareas INA
 urllib.request.urlretrieve('https://alerta.ina.gob.ar/ina/42-RIODELAPLATA/productos/Prono_SanFernando.png',
@@ -17,15 +18,31 @@ print('Abriendo navegador, yendo a Windguru y esperando 20 segundos')
 driver.get('https://www.windguru.cz/632702')
 time.sleep(20)
 driver.save_screenshot('/home/facundol/deltix/windguru.png')
-#driver.save_screenshot('/home/facundol/deltix/screenshot_windguru.png')
-#print('Taking screenshot')
-#x = 1
-#y = 220
-#width = 840
-#height = 320
 
-#full_screenshot = Image.open('/home/facundol/deltix/screenshot_windguru.png')
-#cropped_area = full_screenshot.crop((x, y, x + width, y + height))
-#cropped_area.save('/home/facundol/deltix/windguru.png')
-print('Cropping and saving the image')
+driver.get('https://www.hidro.gov.ar/oceanografia/pronostico.asp')
+
+time.sleep(10)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+# Find the table
+table = soup.find('table')
+
+# Extract table data
+table_data = []
+if table:
+    rows = table.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        table_data.append(cols)
+
+print(table_data)
+
+table_text = '\n'.join(['\t'.join(row) for row in table_data])
+
+with open('/home/facundol/deltix/table_data.txt', 'w') as file:
+    file.write(table_text)
+
+
 driver.quit()
